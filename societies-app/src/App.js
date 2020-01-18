2c1be040caf5492154584ff6d36c6dfc177bed26
Shubham Jain <shubham.jain@hotmail.com>
2020-01-18 15:14:43 +0000

    Cleanup

--- societies-app/src/App.js
+++ societies-app/src/App.js
@@ -5,23 +5,11 @@ import {
   Route,
   Link
 } from "react-router-dom";
-<<<<<<< Updated upstream
-import Societies from "./Societies"
-=======
 
+import Societies from "./Societies"
 import Events from "./Events";
 
->>>>>>> Stashed changes
-// This site has 3 pages, all of which are rendered
-// dynamically in the browser (not server rendered).
-//
-// Although the page does not ever refresh, notice how
-// React Router keeps the URL up to date as you navigate
-// through the site. This preserves the browser history,
-// making sure things like the back button and bookmarks
-// work properly.
-
-export default function BasicExample() {
+export default function SocietiesApp() {
   return (
     <Router>
       <div>
@@ -33,19 +21,12 @@ export default function BasicExample() {
             <Link to="/societies">Societies</Link>
           </li>
           <li>
-            <Link to="/events">events</Link>
+            <Link to="/events">Events</Link>
           </li>
         </ul>
 
         <hr />
 
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
         <Switch>
           <Route exact path="/">
             <Home />
@@ -62,21 +43,11 @@ export default function BasicExample() {
   );
 }
 
-// You can think of these components as "pages"
-// in your app.
-
+// You can think of these components as "pages" in your app
 function Home() {
   return (
     <div>
       <h2>Home</h2>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
     </div>
   );
 }
