2364f05c51576d6b7784d1c3be2a98644af8c9a1
Jeremy <jeremyloyingping@googlemail.com>
2020-01-18 14:30:52 +0000

    Nish 2

--- server.js
+++ server.js
@@ -6,12 +6,4 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => res.send('Hello, world!'));
 
-app.get('/', function (req, res) {
-    // do complicated stuff
-
-
-
-    return res.send('Hello, world!');
-});
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
