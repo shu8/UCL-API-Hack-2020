27269d73a5ecae8dc0fe98ab7bbaca7c4e8699f1
Jeremy <jeremyloyingping@googlemail.com>
2020-01-18 14:37:36 +0000

    Nish 3

--- server.js
+++ server.js
@@ -3,14 +3,11 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
-<<<<<<< HEAD
-=======
 
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + '/views/index.html');
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/views/index.html');
 });
->>>>>>> 80be58e65e2141f3abb944e81bd62b9c15626db9
 
-app.get('/', (req, res) => res.send('Hello, world!'));
+app.get('/api', (req, res) => res.send('Hello, world!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`UCL API Hack 2020 app listening on port ${port}!`));
