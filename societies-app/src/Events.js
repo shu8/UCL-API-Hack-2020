aa000b73e0957944137dbcdb22d54b2429f54d72
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2020-01-18 15:52:34 +0000

    add card

--- societies-app/src/Events.js
+++ societies-app/src/Events.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+import {
+    Card,
+    Button
+} from 'react-bootstrap';
+
 export default class Events extends React.Component{
     constructor (props) {
         super(props)
@@ -10,8 +15,23 @@ export default class Events extends React.Component{
     componentDidMount() {
         //alert ("hello");
     }
+
     render () {
-        return <h1>Events</h1>
+        return (
+            
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Event 1</Card.Title>
+                    <Card.Text> Society it's run by <br/>
+                    Event date <br/>
+                    This is where the event preview will go. <br/>
+                    </Card.Text>
+                    <Button variant="primary"> Go to event page </Button>
+                </Card.Body>
+                </Card>
+            
+)
     }
 }
 
