b81e78de64b7a9eaaf1e580693fa8dc747e221d9
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2020-01-18 16:15:44 +0000

    events webpage

--- societies-app/src/Events.js
+++ societies-app/src/Events.js
@@ -9,7 +9,20 @@ export default class Events extends React.Component{
     constructor (props) {
         super(props)
         this.state = {
-            events: []
+            events: [
+                {
+                    event_name : "Sign Language Lesson 4", 
+                    society : "Sign Language Society",
+                    date : "12/2/20",
+                    event_preview : "Learn  some sign language in a relaxed setting!"
+                },
+                {
+                    event_name : "Ratatouille Screening", 
+                    society : "Film and Media Society",
+                    date : "19/2/20", 
+                    event_preview : "Watch a MASTERPIECE with free food!"
+                }
+            ]
         }
     }
     componentDidMount() {
@@ -17,21 +30,21 @@ export default class Events extends React.Component{
     }
 
     render () {
-        return (
-            
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
-                <Card.Body>
-                    <Card.Title>Event 1</Card.Title>
-                    <Card.Text> Society it's run by <br/>
-                    Event date <br/>
-                    This is where the event preview will go. <br/>
-                    </Card.Text>
-                    <Button variant="primary"> Go to event page </Button>
-                </Card.Body>
+        return this.state.events.map(event => {
+            return (
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title> {event.event_name}</Card.Title>
+                        <Card.Text> {event.society} <br/>
+                        {event.date} <br/>
+                        {event.event_preview} <br/>
+                        </Card.Text>
+                        <Button variant="primary"> Go to event page </Button>
+                    </Card.Body>
                 </Card>
-            
-)
+            )
+        });
     }
 }
 
