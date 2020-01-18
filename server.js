760e9cc32d631801bb33431ed12116a0b298d191
Shubham Jain <shubham.jain@hotmail.com>
2020-01-18 14:07:10 +0000

    Serve static website from express

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('public'));
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + '/views/index.html');
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
