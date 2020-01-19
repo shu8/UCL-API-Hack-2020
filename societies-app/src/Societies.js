17643ef177c49e477b5ddfbe011dd1a70dc7dcac
Nishan Sen <nishan.sen.19@ucl.ac.uk>
2020-01-19 10:23:24 +0000

    added toggle button

--- societies-app/src/Societies.js
+++ societies-app/src/Societies.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {
-  Card
+  Card,
+  Button
 } from "react-bootstrap"
 
 export default class Interests extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      view_mode: "categories",
       categories: [
         {
           name: "Technology",
@@ -44,7 +46,58 @@ export default class Interests extends React.Component {
     // TODO call API, get societies, store in state
   }
 
+  renderCat() {
+    return (
+      <div>
+        <h1 style={{ textAlign: 'center' }}>
+          Societies
+          <Button variant="primary" className="float-right" onClick={() => this.setState({ view_mode: "all"})}>Toggle</Button>
+        </h1>
+        {this.state.categories.map((cat, i) => {
+          return (
+            <Card bg="light" style={{ width: '100%' }} onClick={() => this.props.history.push(`/societies/${cat.name}`)}>
+              <Card.Body>
+                <Card.Title>{cat.name}</Card.Title>
+                <Card.Img src={cat.logo} style={{ width: '50%' }} align='right'></Card.Img>
+              </Card.Body>
+            </Card>
+          );
+        })}
+      </div>
+    );
+  }
+
+  renderAll() {
+    return (
+      <div>
+        <h1 style={{ textAlign: 'center' }}>
+          Societies
+          <Button variant="primary" className="float-right" onClick={() => this.setState({ view_mode: "categories"})}>Toggle</Button>
+        </h1>
+        {this.state.categories.map((cat, i) => {
+          return (
+            <Card bg="light" style={{ width: '100%' }} onClick={() => this.props.history.push(`/societies/${cat.name}`)}>
+              <Card.Body>
+                <Card.Title>{cat.name}</Card.Title>
+                <Card.Img src={cat.logo} style={{ width: '50%' }} align='right'></Card.Img>
+              </Card.Body>
+            </Card>
+          );
+        })}
+      </div>
+    );
+  }
+
+  handleLoginClick() {
+    this.setState({view_mode: "categories"});
+  }
+
+  handleLogoutClick() {
+    this.setState({view_mode: "all"});
+  }
+
   render() {
+    const { view_mode } = this.state //destucture state
     if (this.props.match.params.category) {
       return (
         <div>
@@ -52,27 +105,14 @@ export default class Interests extends React.Component {
             Societies
             {` (${this.props.match.params.category})`}
           </h1>
-          SOCIETIES IN THIS CATEGORY
         </div>
       )
     } else {
-      return (
-        <div>
-          <h1 style={{ textAlign: 'center' }}>
-            Societies
-          </h1>
-          {this.state.categories.map((cat, i) => {
-            return (
-              <Card bg="light" style={{ width: '100%' }} onClick={() => this.props.history.push(`/societies/${cat.name}`)}>
-                <Card.Body>
-                  <Card.Title>{cat.name}</Card.Title>
-                  <Card.Img src={cat.logo} style={{ width: '50%' }} align='right'></Card.Img>
-                </Card.Body>
-              </Card>
-            );
-          })}
-        </div>
-      )
+      if (view_mode=="categories") {
+        return this.renderCat();
+      } else if(view_mode=="all") {
+        return this.renderAll();
+      }
     }
   }
 }
