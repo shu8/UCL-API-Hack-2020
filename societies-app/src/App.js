4cc865be1f3b949712e22b9736e29c1b113b73fe
Shubham Jain <shubham.jain@hotmail.com>
2020-01-19 09:48:05 +0000

    Revamp societies page

--- societies-app/src/App.js
+++ societies-app/src/App.js
@@ -10,25 +10,30 @@ import Home from "./Home";
 import Societies from "./Societies";
 import Events from "./Events";
 import Admin from "./Admin";
+import Interests from "./Interests";
 
 export default function SocietiesApp() {
   return (
     <Router>
       <div>
-            <Link className= "navbar" to="/">Home</Link>
-            <Link className= "navbar" to="/societies">Societies</Link>
-            <Link className= "navbar" to="/events">Events</Link>
+        <Link className= "navbar" to="/">Home</Link>
+        <Link className= "navbar" to="/societies">Societies</Link>
+        <Link className= "navbar" to="/events">Events</Link>
         <hr />
-
-        
 
         <Switch>
           <Route exact path="/">
             <Home />
           </Route>
-          <Route path="/societies">
-            <Societies />
-          </Route>
+
+          <Route
+            path="/societies/:category"
+            component={Societies}
+          />
+          <Route path="/societies"
+            component={Societies}
+          />
+
           <Route path="/events">
             <Events />
           </Route>
--- societies-app/src/Societies.js
+++ societies-app/src/Societies.js
@@ -1,91 +1,78 @@
 import React from 'react';
 import {
-    Card,
-    Button,
-    Modal
+  Card
 } from "react-bootstrap"
 
-export default class Societies extends React.Component {
-
-    constructor(props) {
-        super(props)
-        this.state = {
-            show: false,
-            openSocIndex: 0,
-            eventsForOpenSoc :[],
-
-            societies: [{
-                name: "s1",
-                desc: "s1 desc",
-                detail: "s1 detailed summary",
-                logo: "/uclsslogo.png"
-            }, {
-                name: "s2",
-                desc: "s2 desc",
-                detail: "s2 detailed summary",
-                logo: "/uclsslogo.png"
-            }, {
-                name: "s3",
-                desc: "s3 desc",
-                detail: "s3 detailed summary",
-                logo: "/uclsslogo.png"
-            }]
+export default class Interests extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [
+        {
+          name: "Technology",
+          logo: "/uclsslogo.png"
+        },
+        {
+          name: "Medicine",
+          logo: "/uclsslogo.png"
+        },
+        {
+          name: "Music",
+          logo: "/uclsslogo.png"
+        },
+        {
+          name: "Sports",
+          logo: "/uclsslogo.png"
+        },
+        {
+          name: "Educational",
+          logo: "/uclsslogo.png"
+        },
+        {
+          name: "Fan-clubs",
+          logo: "/uclsslogo.png"
+        },
+        {
+          name: "Free",
+          logo: "/uclsslogo.png"
         }
+      ]
     }
+  }
 
-    followSociety(socIndex) {
-        console.log(socIndex);
-        // TODO API call, with followed society id
+  componentDidMount() {
+    // TODO call API, get societies, store in state
+  }
+
+  render() {
+    if (this.props.match.params.category) {
+      return (
+        <div>
+          <h1 style={{ textAlign: 'center' }}>
+            Societies
+            {` (${this.props.match.params.category})`}
+          </h1>
+          SOCIETIES IN THIS CATEGORY
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h1 style={{ textAlign: 'center' }}>
+            Societies
+          </h1>
+          {this.state.categories.map((cat, i) => {
+            return (
+              <Card bg="light" style={{ width: '100%' }} onClick={() => this.props.history.push(`/societies/${cat.name}`)}>
+                <Card.Body>
+                  <Card.Title>{cat.name}</Card.Title>
+                  <Card.Img src={cat.logo} style={{ width: '50%' }} align='right'></Card.Img>
+                </Card.Body>
+              </Card>
+            )
+          })}
+        </div>
+      )
     }
-
-    componentDidMount() {
-        // TODO API call, set setState({societies: [api result]})
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>
-                    Societies
-                </h1>
-                <img className='center-img' src="images/societies-logo.png" style={{width: '100%'}}></img>
-                {this.state.societies.map((soc, i) => {
-                    return (
-                        <Card>
-                            <Card variant="top" src="holder.js/100px180" />
-                            <Card.Body>
-                                <Card.Title>{soc.name}</Card.Title>
-                                <Card.Text><i>{soc.desc}</i></Card.Text>
-                                <Card.Img  src= {soc.logo} style= {{width: '50%'}} align = 'right'></Card.Img>
-                                <Button variant="primary" onClick={() => this.setState({ show: true, openSocIndex: i })}>View society</Button>
-                                <br /><br />
-                                <Button variant="success" onClick={() => this.followSociety(i)}>Follow society</Button>
-                            </Card.Body>
-                        </Card>
-
-                    )
-                })}
-
-                <Modal show={this.state.show} onHide={() => this.setState({show: false})}>
-                    <Modal.Header closeButton>
-
-                    <Modal.Title>
-                        {this.state.societies[this.state.openSocIndex].name}
-                        <img src = {this.state.societies[this.state.openSocIndex].logo} style= {{width: '25%', float: 'right'}} />
-
-                        </Modal.Title>
-
-                    </Modal.Header>
-                    <Modal.Body>{this.state.societies[this.state.openSocIndex].detail}</Modal.Body>
-                    <Modal.Footer>
-                    <Button variant="secondary" onClick={() => this.setState({show: false})}>
-                        Close
-                    </Button>
-                    </Modal.Footer>
-                </Modal>
-            </div>
-
-        );
-    }
+  }
 }
-
