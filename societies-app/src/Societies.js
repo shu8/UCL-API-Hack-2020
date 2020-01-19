43731332920cc0905e047a939d887a7c10e3ccac
Shubham Jain <shubham.jain@hotmail.com>
2020-01-19 11:37:17 +0000

    Fix QR; Link to individual soc

--- societies-app/src/Societies.js
+++ societies-app/src/Societies.js
@@ -13,18 +13,21 @@ export default class Societies extends React.Component {
       show: false,
       openSocIndex: 0,
       societies: [{
+        id: "123",
         name: "s1",
         desc: "s1 desc",
         categories: ["Technology", "Medicine"],
         detail: "s1 detailed summary",
         logo: "/uclsslogo.png"
       }, {
+        id: "124",
         name: "s2",
         desc: "s2 desc",
         categories: ["Medicine"],
         detail: "s2 detailed summary",
         logo: "/uclsslogo.png"
       }, {
+        id: "125",
         name: "s3",
         desc: "s3 desc",
         categories: ["Sports"],
@@ -68,10 +71,10 @@ export default class Societies extends React.Component {
     // TODO call API, get societies, store in state
   }
 
-  
+
   renderAllCats() {
     return (
-      <div>       
+      <div>
         <h1 style={{ textAlign: 'center' }}>
           Societies
           <Button variant="primary" className="float-right" onClick={() => this.setState({ view_mode: "all"})}>Toggle</Button>
@@ -135,6 +138,42 @@ export default class Societies extends React.Component {
     );
   }
 
+  renderSoc(desiredSoc) {
+    const socIndex = this.state.societies.findIndex(s => s.id === desiredSoc);
+    const soc = this.state.societies[socIndex];
+    return (
+      <div>
+        <Card>
+          <Card variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>{soc.name}</Card.Title>
+            <Card.Text><i>{soc.desc}</i></Card.Text>
+            <Card.Img src={soc.logo} style={{ width: '50%' }} align='right'></Card.Img>
+            <Button variant="primary" onClick={() => this.setState({ show: true, openSocIndex: socIndex })}>View society</Button>
+            <br /><br />
+            <Button variant="success" onClick={() => this.followSociety(socIndex)}>Follow society</Button>
+          </Card.Body>
+        </Card>
+        <Modal show={this.state.show} onHide={() => this.setState({ show: false })}>
+          <Modal.Header closeButton>
+
+            <Modal.Title>
+              {this.state.societies[this.state.openSocIndex].name}
+              <img src={this.state.societies[this.state.openSocIndex].logo} style={{ width: '25%', float: 'right' }} />
+            </Modal.Title>
+
+          </Modal.Header>
+          <Modal.Body>{this.state.societies[this.state.openSocIndex].detail}</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={() => this.setState({ show: false })}>
+              Close
+                </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
+
   renderAll() {
     return (
       <div>
@@ -162,7 +201,6 @@ export default class Societies extends React.Component {
                 <Modal.Title>
                   {this.state.societies[this.state.openSocIndex].name}
                   <img src={this.state.societies[this.state.openSocIndex].logo} style={{ width: '25%', float: 'right' }} />
-
                 </Modal.Title>
 
               </Modal.Header>
@@ -192,10 +230,12 @@ export default class Societies extends React.Component {
     const { view_mode } = this.state //destucture state
     if (this.props.match.params.category) {
       return this.renderCat(this.props.match.params.category);
+    } else if (this.props.match.params.id) {
+      return this.renderSoc(this.props.match.params.id);
     } else {
       if (view_mode=="categories") {
         return this.renderAllCats();
-      } else if(view_mode=="all") {
+      } else if (view_mode=="all") {
         return this.renderAll();
       }
     }
